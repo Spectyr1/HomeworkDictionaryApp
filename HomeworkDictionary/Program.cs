15bdee02660dc04963e8bd19c7da1312a323194d
Danny Warren <danny@theforbiddenarts.com>
2022-03-24 23:26:33 -0400

    submit

--- HomeworkDictionary/Program.cs
+++ HomeworkDictionary/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Dictionary<int, string> employees = new Dictionary<int, string>();
+bool validId;
+int id;
+string name;
+
+employees.Add(101, "Tim");
+employees.Add(102, "Bob");
+employees.Add(103, "Sue");
+
+do
+{
+    Console.Write("Enter an employee ID # to get the name of the employee: ");
+    string idText = Console.ReadLine();
+    bool validNum = Int32.TryParse(idText, out id);
+
+    validId = employees.TryGetValue(id, out name);
+
+    if (!validId)
+    {
+        Console.WriteLine("The ID entered is not a valid ID.");
+    }
+
+} while (validId != true);
+
+Console.WriteLine($"The employee with ID # {id} is {name}.");
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 HomeworkDictionaryApp
+
+Create a Dictionary list of employee IDs and the name that goes with the ID.
+Fill in a few records. Then ask the user for thier ID and return thier name.
+
